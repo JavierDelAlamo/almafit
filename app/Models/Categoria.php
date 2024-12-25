6fbd6c4f2f4945b90e57b51fd6304adc8fe8e617
Javier <proyectouax@hotmail.com>
2024-12-25 19:25:30 +0100

    Relaciones Categoria ✅

--- app/Models/Categoria.php
+++ app/Models/Categoria.php
@@ -2,9 +2,17 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Categoria extends Model
 {
-    //
+    use HasFactory;
+    //nos permite añadir campos masivos de pruebas.
+    protected $guarded = [];
+    //Una Categoria tiene varias Entradas
+    public function entradas(): HasMany{
+        return $this->hasMany(Entrada::class);
+    }
 }
