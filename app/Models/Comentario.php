2654b993e98aad5b69e895cc41851e62383e9c1a
Javier <proyectouax@hotmail.com>
2024-12-25 19:35:09 +0100

    Relaciones Comentario y User ✅

--- app/Models/Comentario.php
+++ app/Models/Comentario.php
@@ -2,9 +2,19 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Comentario extends Model
 {
-    //
+    use HasFactory;
+    protected $guarded =[];
+    
+    //Un Comentario pertenece a una Entrada
+    public function entrada():BelongsTo
+    {
+        return $this->belongsTo(Entrada::class);
+    }
+
 }
