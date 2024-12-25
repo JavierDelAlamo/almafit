212ce64c1f7bcd2a5f32f8a775a050506723a572
Javier <proyectouax@hotmail.com>
2024-12-25 19:50:48 +0100

    Recurso Categoria, Entrada y Comentario 🌟

---
+++ app/Filament/Resources/EntradaResource.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\EntradaResource\Pages;
+use App\Filament\Resources\EntradaResource\RelationManagers;
+use App\Models\Entrada;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class EntradaResource extends Resource
+{
+    protected static ?string $model = Entrada::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\Select::make('user_id')
+                    ->relationship('user', 'name')
+                    ->required(),
+                Forms\Components\Select::make('categoria_id')
+                    ->relationship('categoria', 'id')
+                    ->required(),
+                Forms\Components\TextInput::make('titulo')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('slug')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\Textarea::make('cuerpo')
+                    ->required()
+                    ->columnSpanFull(),
+                Forms\Components\FileUpload::make('image_url')
+                    ->image()
+                    ->required(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('user.name')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('categoria.id')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('titulo')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('slug')
+                    ->searchable(),
+                Tables\Columns\ImageColumn::make('image_url'),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListEntradas::route('/'),
+            'create' => Pages\CreateEntrada::route('/create'),
+            'edit' => Pages\EditEntrada::route('/{record}/edit'),
+        ];
+    }
+}
