2484917901d27c174150c4464c192e017d2c125a
Javier <proyectouax@hotmail.com>
2024-12-28 13:14:12 +0100

    Creamos Modelo Usuario, (campos, formulario, tabla)  👷

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -4,14 +4,18 @@ namespace App\Filament\Resources;
 
 use App\Filament\Resources\UserResource\Pages;
 use App\Filament\Resources\UserResource\RelationManagers;
-use App\Models\User;
+use App\Models\User; //Añadimos la clase User
+use App\Models\Role;
 use Filament\Forms;
+use Filament\Forms\Components\TextInput; //Añadimos la clase TextInput
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
-use Filament\Tables;
-use Filament\Tables\Table;
+use Filament\Tables; //Añadimos la clase Tables
+use Filament\Tables\Columns\TextColumn; //Añadimos la clase TextColumn
+use Filament\Tables\Table; //Añadimos la clase Table
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Filament\Forms\Components\Select; //Añadimos la clase Select
 
 class UserResource extends Resource
 {
@@ -19,19 +23,61 @@ class UserResource extends Resource
 
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
 
-    public static function form(Form $form): Form
+    protected static ?string $navigationLabel = 'Usuarios'; //Añadimos la etiqueta de navegación
+
+    protected static ?string $label = 'Usuario';  //Añadimos la etiqueta de la página
+
+    public static function form(Form $form): Form 
     {
         return $form
             ->schema([
-                //
-            ]);
+                //Creamos los campos del formulario con los datos de la bbdd
+                TextInput::make('name')
+                    ->label('Nombre') //Añadimos una etiqueta
+                    ->required(), //Validamos que sea requerido
+                TextInput::make('email')
+                    ->label('Correo electrónico') //Añadimos una etiqueta
+                    ->email() //Validamos que sea un email
+                    ->unique(ignoreRecord: true) //Validamos que sea único, ignorando el registro actual
+                    //podemos modificar el usuario manteniendo el email y volviendo a registrarlo.
+                    ->required(), //Validamos que sea requerido
+                TextInput::make('password')
+                    ->label('Contraseña')
+                    ->hiddenOn('edit') //Ocultamos el campo en el formulario de edición
+                    ->password() //Ocultamos el texto
+                    ->required() //Validamos que sea requerido
+                        ->autocomplete('new-password') //Desactivamos el autocompletado
+                        ->default('almafit1234'), //Valor por defecto
+    
+                    //Añadimos un campo select para el rol
+                    Select::make('roles')
+                        ->multiple()
+                        ->relationship('roles', 'name')
+                        ->label('Rol')
+                        ->required()
+                        ->options(Role::all()->pluck('name', 'id')),
+                    
+            ]);      
     }
 
     public static function table(Table $table): Table
     {
         return $table
             ->columns([
-                //
+                TextColumn::make('name') //Añadimos una columna con el nombre
+                    ->label('Nombre'),
+                TextColumn::make('email') //Añadimos una columna con el email
+                    ->label('Correo electrónico'),
+                TextColumn::make('email_verified_at') //Añadimos una columna con la fecha de verificación del email
+                    ->label('Verificado el'),
+                TextColumn::make('roles.name') //Añadimos una columna con el rol
+                    ->label('Rol'), 
+
+
+
+
+
+
             ])
             ->filters([
                 //
--- app/Models/User.php
+++ app/Models/User.php
@@ -7,11 +7,13 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Spatie\Permission\Traits\HasRoles; //Añadimos la clase HasRoles de Spatie
+use Laravel\Sanctum\HasApiTokens; // Añadimos la clase HasApiTokens de Laravel Sanctum
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use  HasFactory, Notifiable, HasRoles; //Añadimos HasRoles
 
     /**
      * The attributes that are mass assignable.
@@ -22,6 +24,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        
     ];
 
     /**
